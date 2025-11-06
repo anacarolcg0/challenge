eebcba1f3104016f7b28e56df4e0f84f4614adaf
anacarolcg0 <anacarolcg0@gmail.com>
2025-11-06 11:57:32 -0300

    arrumando para sprint 4.

--- src/main/java/br/com/fiap/challenge/configuration/InternacionalizacaoConfig.java
+++ src/main/java/br/com/fiap/challenge/configuration/InternacionalizacaoConfig.java
@@ -1,4 +1,4 @@
-package br.com.fiap.challenge.configuration;
+package br.com.fiap.spring_mvc.configuration;
 
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
@@ -7,25 +7,25 @@ import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.i18n.FixedLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 import java.nio.charset.StandardCharsets;
 import java.util.Locale;
 
 @Configuration
 public class InternacionalizacaoConfig implements WebMvcConfigurer {
-
     @Bean
     public LocaleResolver localeResolver() {
-        // Força o idioma padrão como português do Brasil
-        return new FixedLocaleResolver(new Locale("pt", "BR"));
+        SessionLocaleResolver slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(new Locale("pt", "BR"));
+        return slr;
     }
 
     @Bean
     public LocaleChangeInterceptor localeChangeInterceptor() {
         LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
-        lci.setParamName("lang"); // permite trocar idioma com ?lang=en
+        lci.setParamName("Long");
         return lci;
     }
 
@@ -37,8 +37,8 @@ public class InternacionalizacaoConfig implements WebMvcConfigurer {
     @Bean
     public MessageSource messageSource() {
         ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
-        messageSource.setBasename("i18n/messages");
-        messageSource.setDefaultEncoding(StandardCharsets.UTF_8.name());
+        messageSource.setBasename("18n/messages");
+        messageSource.setDefaultEncoding(StandardCharsets.ISO_8859_1.name());
         return messageSource;
     }
 }
